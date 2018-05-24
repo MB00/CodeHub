746ac4889587ebb05ab43c87f972bb21e067f011
MB00 <javac917@gmail.com>
2018-05-23 23:09:28 -0400

    Added one-line limit and soft keyboard listener to Search

--- app/src/main/java/mb00/android/codehub/ui/search/view/SearchActivity.java
+++ app/src/main/java/mb00/android/codehub/ui/search/view/SearchActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import mb00.android.codehub.R;
 import mb00.android.codehub.data.BundleKeys;
@@ -25,14 +27,13 @@ public class SearchActivity extends NavigationDrawerActivity {
     // SearchActivity fields
     //==============================================================================================
 
+    public static String searchQuery;
+    private Bundle queryBundle;
+
     private EditText searchQueryEditText;
     private ImageButton backButton;
     private ImageButton searchButton;
-    public static String searchQuery;
-    private Bundle queryBundle;
     private ViewPager searchViewPager;
-    private PagerAdapter searchPagerAdapter;
-    private TabLayout searchTabLayout;
 
     //==============================================================================================
     // Activity / lifecycle methods
@@ -44,20 +45,27 @@ public class SearchActivity extends NavigationDrawerActivity {
         setContentView(R.layout.activity_search);
         super.setupNavigationDrawer(this);
 
+        initialize();
+        setListeners();
+    }
+
+    private void initialize() {
+        searchQueryEditText = findViewById(R.id.search_query_edit_text);
+        backButton = findViewById(R.id.search_back_button);
+        searchButton = findViewById(R.id.main_search_button);
+        searchViewPager = findViewById(R.id.search_view_pager);
+        TabLayout searchTabLayout = findViewById(R.id.search_tab_layout);
+
         queryBundle = new Bundle();
         queryBundle.putString(BundleKeys.SEARCH_QUERY_KEY, searchQuery);
 
-        backButton = findViewById(R.id.search_back_button);
-        searchQueryEditText = findViewById(R.id.search_query_edit_text);
-        searchButton = findViewById(R.id.main_search_button);
-        searchPagerAdapter = new SearchFragmentPagerAdapter(getSupportFragmentManager(), this, queryBundle);
-        searchViewPager = findViewById(R.id.search_view_pager);
-        searchTabLayout = findViewById(R.id.search_tab_layout);
-
+        PagerAdapter searchPagerAdapter = new SearchFragmentPagerAdapter(getSupportFragmentManager(), this, queryBundle);
         searchViewPager.setAdapter(searchPagerAdapter);
         searchViewPager.setOffscreenPageLimit(3);
         searchTabLayout.setupWithViewPager(searchViewPager);
+    }
 
+    private void setListeners() {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,17 +73,28 @@ public class SearchActivity extends NavigationDrawerActivity {
             }
         });
 
-        searchQuery = "";
-        queryBundle.putString(BundleKeys.SEARCH_QUERY_KEY, searchQuery);
+        searchQueryEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                search();
+                return true;
+            }
+        });
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                searchQuery = searchQueryEditText.getText().toString();
-                queryBundle.putString(BundleKeys.SEARCH_QUERY_KEY, searchQuery);
-                searchViewPager.getAdapter().notifyDataSetChanged();
+                search();
             }
         });
+    }
+
+    private void search() {
+        searchQuery = "";
+        queryBundle.putString(BundleKeys.SEARCH_QUERY_KEY, searchQuery);
+        searchQuery = searchQueryEditText.getText().toString();
+        queryBundle.putString(BundleKeys.SEARCH_QUERY_KEY, searchQuery);
+        searchViewPager.getAdapter().notifyDataSetChanged();
     }
 
 }
